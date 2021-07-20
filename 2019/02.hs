92f392654908118d48398ca0e6106def5aeb64c9
kraftwerk28 <kefirchik3@gmail.com>
2021-07-20 21:19:41 +0300

    Move Intcode computer to module

--- 2019/02.hs
+++ 2019/02.hs
@@ -1,37 +1,20 @@
 import           Data.List.Split                ( splitOn )
-
-data State = State [Int] Int
-  deriving Show
-
-setAt :: Int -> Int -> [Int] -> [Int]
-setAt index value list =
-  let (l, r') = splitAt index list in l ++ [value] ++ tail r'
-
-runProgram :: State -> State
-runProgram state@(State code ip) = case opcode of
-  1 ->
-    let a = code !! (code !! (ip + 1))
-        b = code !! (code !! (ip + 2))
-        c = (code !! (ip + 3))
-    in  runProgram $ State (setAt c (a + b) code) (ip + 4)
-  2 ->
-    let a = code !! (code !! (ip + 1))
-        b = code !! (code !! (ip + 2))
-        c = (code !! (ip + 3))
-    in  runProgram $ State (setAt c (a * b) code) (ip + 4)
-  _ -> state
-  where opcode = code !! ip
+import           Intcode                        ( State(..)
+                                                , newState
+                                                , runProgram
+                                                , setAt
+                                                )
 
 main = do
   nums <- map read . splitOn "," <$> getContents
-  let preparedCode            = setAt 1 12 $ setAt 2 2 nums
-      finState@(State code _) = runProgram (State preparedCode 0)
+  let preparedCode                = setAt 1 12 $ setAt 2 2 nums
+      finState@(State code _ _ _) = runProgram (newState preparedCode [])
   print $ head code
   let
-    State (_ : noun : verb : _) _ =
+    State (_ : noun : verb : _) _ _ _ =
       head
-        . filter ((== 19690720) . (\(State (res : _) _) -> res))
-        . map (runProgram . (`State` 0))
+        . filter ((== 19690720) . (\(State (res : _) _ _ _) -> res))
+        . map (runProgram . (`newState` []))
         $ [ setAt 1 x $ setAt 2 y preparedCode
           | x <- [1 .. 99]
           , y <- [1 .. 99]
